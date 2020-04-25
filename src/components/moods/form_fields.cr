2a608c2a69f0c75fb8df00461ff1ef078773362b
Jade D. Kharats <jade.kharats@gmail.com>
2020-04-25 16:12:01 +0200

    :wastebasket: clean comment lines

--- src/components/moods/form_fields.cr
+++ src/components/moods/form_fields.cr
@@ -2,9 +2,7 @@ class Moods::FormFields < BaseComponent
   needs operation : SaveMood
 
   def render
-    # mount Shared::Field.new(operation.user_id), &.text_input(autofocus: "true")
     mount Shared::Field.new(operation.level), &.text_input(autofocus: "true")
-    # mount Shared::Field.new(operation.time)
     mount Shared::Field.new(operation.comment)
   end
 end
