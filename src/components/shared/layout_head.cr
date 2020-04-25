09e205457f179c95024bf9b05b9658938018664b
Jade D. Kharats <jade.kharats@gmail.com>
2020-04-25 21:51:23 +0200

    :wrench: Change manifest.json for pwa

--- src/components/shared/layout_head.cr
+++ src/components/shared/layout_head.cr
@@ -10,6 +10,7 @@ class Shared::LayoutHead < BaseComponent
       css_link asset("css/app.css"), data_turbolinks_track: "reload"
       js_link asset("js/app.js"), defer: "true", data_turbolinks_track: "reload"
       meta name: "turbolinks-cache-control", content: "no-cache"
+      raw "<link rel=\"manifest\" href=\"/manifest.json\">"
       csrf_meta_tags
       responsive_meta_tag
     end
