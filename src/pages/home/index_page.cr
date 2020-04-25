25f5d51363bd8fd952d4849ccb2738a0687fc507
Jade D. Kharats <jade.kharats@gmail.com>
2020-04-25 17:15:09 +0200

    :lipstick: Begin main css

--- src/pages/home/index_page.cr
+++ src/pages/home/index_page.cr
@@ -1,6 +1,22 @@
 class Home::IndexPage < AuthLayout
   def content
+    tag("section", class: "main") do
+      background
+      title
+      button
+    end
+  end
+
+  private def background
+    div(class: "background-image")
+  end
+
+  private def title
     h1 "Bienvenue Sur BOM3"
-    link "Se connecter", to: SignIns::New
+    h3 "Comment vous sentez-vous?"
+  end
+
+  private def button
+    link "Se connecter", to: SignIns::New, class: "btn"
   end
 end
