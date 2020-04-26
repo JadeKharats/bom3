edbc34961b3f1b77708dce17601a1dfaf38ace31
Jade D. Kharats <jade.kharats@gmail.com>
2020-04-26 12:15:48 +0200

    :children_crossing: Add uniqueness of pseudo

--- src/operations/save_user.cr
+++ src/operations/save_user.cr
@@ -2,4 +2,8 @@ class SaveUser < User::SaveOperation
   # To save user provided params to the database, you must permit them
   # https://luckyframework.org/guides/database/validating-saving#perma-permitting-columns
   permit_columns pseudo
+
+  before_save do
+    validate_uniqueness_of pseudo
+  end
 end
