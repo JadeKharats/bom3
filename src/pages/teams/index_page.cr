d910d952a4093ac01d1add9fda5f1a708bced2dc
Jade D. Kharats <jade.kharats@gmail.com>
2020-04-26 17:21:49 +0200

    :construction: Css carousel

--- src/pages/teams/index_page.cr
+++ src/pages/teams/index_page.cr
@@ -10,25 +10,35 @@ class Teams::IndexPage < MainLayout
   end
 
   def render_teams
-    div(class: "tile is-parent") do
-      tag("articles", class: "tile is-child box") do
-        tag("p",class: "title has-text-primary") do
-          text "les équipes"
-        end
-        div(class: "content") do
-          @teams.each do |team|
-            div(class: "box has-text-light has-background-info") do
-              link to: Teams::Show.with(team), class: "has-text-light" do
-                tag("article", class: "media") do
-                  div(class: "media-content") do
-                    text team.name
-                  end
-                end
-              end
-            end
-          end
+    div(class: "horizontal-scroll-wrapper rectangles") do
+      @teams.each do |team|
+        div do
+          text "#{team.name}"
         end
       end
     end
   end
+
+  # def render_teams
+  #   div(class: "tile is-parent") do
+  #     tag("articles", class: "tile is-child box") do
+  #       tag("p",class: "title has-text-primary") do
+  #         text "les équipes"
+  #       end
+  #       div(class: "content") do
+  #         @teams.each do |team|
+  #           div(class: "box has-text-light has-background-info") do
+  #             link to: Teams::Show.with(team), class: "has-text-light" do
+  #               tag("article", class: "media") do
+  #                 div(class: "media-content") do
+  #                   text team.name
+  #                 end
+  #               end
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
