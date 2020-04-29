51d01968a734b895e3e43507ee7366ff58c0096b
Jade D. Kharats <jade.kharats@gmail.com>
2020-04-29 09:35:52 +0200

    :package: Update bulma package

--- src/pages/teams/index_page.cr
+++ src/pages/teams/index_page.cr
@@ -3,42 +3,23 @@ class Teams::IndexPage < MainLayout
   quick_def page_title, "All Teams"
 
   def content
-    div(class: "tile is-ancestor") do
+    tag("section") do
       link "Créer un équipe", to: Teams::New, class: "button is-primary is-inverted is-medium"
+    end
+    tag("section") do
       render_teams
     end
   end
 
   def render_teams
-    div(class: "horizontal-scroll-wrapper rectangles") do
+    tag("table", class: "table is-fullwidth") do
       @teams.each do |team|
-        div do
-          text "#{team.name}"
+        tag("tr") do
+          tag("td") do
+            text truncate_text(team.name, length: 30)
+          end
         end
       end
     end
   end
-
-  # def render_teams
-  #   div(class: "tile is-parent") do
-  #     tag("articles", class: "tile is-child box") do
-  #       tag("p",class: "title has-text-primary") do
-  #         text "les équipes"
-  #       end
-  #       div(class: "content") do
-  #         @teams.each do |team|
-  #           div(class: "box has-text-light has-background-info") do
-  #             link to: Teams::Show.with(team), class: "has-text-light" do
-  #               tag("article", class: "media") do
-  #                 div(class: "media-content") do
-  #                   text team.name
-  #                 end
-  #               end
-  #             end
-  #           end
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
 end
